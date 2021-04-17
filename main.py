78167f672b4ae3ffc6d6c4bb901f203d122324f3
Marta Sleboda <sleboda.m98@gmail.com>
2021-04-17 22:34:08 +0200

    all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Response, status
-# from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, Response, status
 import hashlib
 
 app = FastAPI()
@@ -9,17 +8,11 @@ def read_root():
     return {"message": "Hello world!"}
 
 @app.get("/auth")
-async def read_items():
-    the_hash = hashlib.sha512( str( password ).encode("utf-8") ).hexdigest()
-    the_pass_check = password_hash.split("&password_hash=",1)[1] 
-    
+def decrypt(response: Response, password: str = '', password_hash: str = '0'):
+    encrypted_password = hashlib.sha512(password.encode("utf-8")).hexdigest()
+    if password_hash == encrypted_password:
+        response.status_code = status.HTTP_204_NO_CONTENT
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
 
-
-# @app.get("/auth")
-# async def read_items(password: str, password_hash: str):
-#     the_hash = hashlib.sha512( str( password ).encode("utf-8") ).hexdigest()
-#     the_pass_check = password_hash.split("&password_hash=",1)[1] 
-#     if the_hash == the_pass_check:
-#         return JSONResponse(status_code=status.HTTP_204_CREATED, content=item)
-#     else:
-#         return JSONResponse(status_code=status.HTTP_401_CREATED, content=item)
+    return response
