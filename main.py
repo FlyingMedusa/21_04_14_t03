7104d5ac9b123df06109a71d5286a7d160bbf8f5
Marta Sleboda <sleboda.m98@gmail.com>
2021-04-18 22:31:24 +0200

    task 4 final update

--- main.py
+++ main.py
@@ -2,7 +2,40 @@ from fastapi import FastAPI, Request, Response, status
 import hashlib
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from datetime import date, timedelta, datetime
+import itertools
 
+
+
+class User(BaseModel):
+    name: str
+    surname: str
+
+class RegisteredUser():
+    newid = itertools.count()
+
+    def __init__(self, name, surname):
+        self.id = next(RegisteredUser.newid)
+        self.name = name
+        self.surname = surname
+        self.register_date = str(date.today())
+        self.vaccination_date = self.generate_vacc_date()
+    
+    def generate_vacc_date(self):
+        delay = len(self.name) + len(self.surname)
+        vaccination_date = datetime.fromisoformat(self.register_date) + timedelta(days=10)
+        return vaccination_date.date()
+    
+    def register_user(self):
+        res = {
+            "id": self.id,
+            "name" : self.name,
+            "surname" : self.surname,
+            "register_date" : self.register_date,
+            "vaccination_date" : self.vaccination_date
+        }
+        return res
+ 
 
 app = FastAPI()
 
@@ -19,7 +52,10 @@ def decrypt(response: Response, password: str = '', password_hash: str = ''):
         response.status_code = status.HTTP_401_UNAUTHORIZED
     return response
 
-@app.put("/register")
-def update_item(name: str, surname: str):
-    json_compatible_item_data = jsonable_encoder(name)
-    return JSONResponse(content=json_compatible_item_data)
+@app.post("/register")
+async def create_user(response: Response, user: User):
+
+    result = RegisteredUser(user.name, user.surname)
+    response.status_code = status.HTTP_201_CREATED
+
+    return result.register_user()
